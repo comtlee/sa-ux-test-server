ba0a657764b3211766aa23f766a62478bbcef086
comt-mix <comt-mix@gmail.com>
2022-11-13 02:21:57 +0900

    ✨Feat: User 스키마 추가

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);

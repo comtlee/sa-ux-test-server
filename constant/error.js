a46edee83e55dab3674e64db424b4d4a729fd3da
comt-mix <comt-mix@gmail.com>
2022-11-13 02:25:31 +0900

    ✨Feat: 에러 상수화

---
+++ constant/error.js
@@ -0,0 +1,8 @@
+const ERROR = {
+  INTERNAL_SERVER_ERROR: "Internal server error",
+  INVALID_TOKEN: "Invalid Token",
+  NOT_FOUND: "Not Found",
+  UNAUTHORIZED_USER: "You are not authorized",
+};
+
+module.exports = ERROR;

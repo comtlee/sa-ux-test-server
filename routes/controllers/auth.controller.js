40974e100d78e1bc723fbedfe56c6fa2afcb807c
comt-mix <comt-mix@gmail.com>
2022-11-13 02:24:12 +0900

    ✨Feat: login 기능 구현

--- routes/controllers/auth.controller.js
+++ routes/controllers/auth.controller.js
@@ -1,3 +1,17 @@
-exports.auth = (req, res, next) => {
-  res.send("auth");
+const User = require("../../models/User");
+
+exports.login = async (req, res, next) => {
+  const { name, email } = req.body;
+
+  try {
+    const user = await User.findOne({ email }).lean();
+
+    if (!user) {
+      await User.create({ name, email });
+    }
+
+    res.json({ result: "success", user });
+  } catch (err) {
+    next(err);
+  }
 };

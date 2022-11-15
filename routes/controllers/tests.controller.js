7036c4bd95b35732a35852faa638b7089c0a7c7d
comt-mix <comt-mix@gmail.com>
2022-11-16 00:40:52 +0900

    ✨Feat: 웹 사이트와 연결하기

--- routes/controllers/tests.controller.js
+++ routes/controllers/tests.controller.js
@@ -1,3 +1,12 @@
-exports.tests = (req, res, next) => {
-  res.send("tests");
+const createError = require("http-errors");
+const ERROR = require("../../constant/error");
+
+exports.getKey = (req, res, next) => {
+  const key = req.query.key;
+
+  if (!key) {
+    next(createError(ERROR.FAILED_REQUEST));
+  }
+
+  res.json({ message: "success" });
 };
